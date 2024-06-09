8bd64100b207671ae785c6251842ce8d2854a8f2
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-06-09 10:47:36 +0300

    no closing of connection

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -7,7 +7,7 @@ import { migrate } from "drizzle-orm/neon-http/migrator";
 console.log("Database_URL:", process.env.DATABASE_URL!);
 async function migration() {
   await migrate(db, { migrationsFolder: __dirname + "/migrations" });
-  // await client.end();
+  // await client.end();  it is serverless so no need of closing connection
   console.log("======== Migrations ended ========");
   process.exit(0);
 }
